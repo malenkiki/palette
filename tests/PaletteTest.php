7596235b7483316b1285f146076d7cb90525c361
malenkiki <petit.michel@gmail.com>
2014-01-09 00:10:29 +0100

    Remove bad test

--- tests/PaletteTest.php
+++ tests/PaletteTest.php
@@ -1198,13 +1198,6 @@ class PaletteTest extends PHPUnit_Framework_TestCase
         $p = new Malenki\Palette($hsv);
         $this->assertEquals(240, $p->complementary()->h);
         
-        $hsv = new \stdClass();
-        $hsv->h = 10;
-        $hsv->s = 1;
-        $hsv->v = 0.5;
-        $p = new Malenki\Palette($hsv);
-        //var_dump($p->complementary()->hsl()); //TODO: bug? or misunderstanding?
-        $this->assertEquals(290, $p->complementary()->hsl()->h);
     }
 
 }
